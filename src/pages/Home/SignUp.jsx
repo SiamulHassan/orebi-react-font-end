1351b4d0f676d1cd90a7ff8efc65917d350270c4
SiamulHassan <syeam45@gmail.com>
2024-03-08 19:49:42 +0600

    json chaged

--- src/pages/Home/SignUp.jsx
+++ src/pages/Home/SignUp.jsx
@@ -81,207 +81,211 @@ const SignUp = () => {
         since the.
       </p>
       <Devider />
-      <form onSubmit={formik.handleSubmit}>
-        <div className="mt-14">
-          <FormLayout formTitle={"Your Personal Details"}>
-            <div className="grid grid-cols-1 gap-x-10 sm:grid-cols-[35%,35%]">
-              <div className="mb-5">
-                <Input
-                  onChange={formik.handleChange}
-                  name="firstName"
-                  value={formik.values.firstName}
-                  as="input"
-                  label="First Name"
-                  placeholder="First Name"
-                  type="text"
-                />
-                <InputError
-                  error={formik.errors.firstName}
-                  touched={formik.touched.firstName}
-                />
-              </div>
-              <div className="mb-5">
-                <Input
-                  onChange={formik.handleChange}
-                  name="lastName"
-                  value={formik.values.lastName}
-                  as="input"
-                  label="Last Name"
-                  placeholder="Last Name"
-                  type="text"
-                />
-                <InputError
-                  error={formik.errors.lastName}
-                  touched={formik.touched.lastName}
-                />
-              </div>
-              <div className="mb-5">
-                <Input
-                  onChange={formik.handleChange}
-                  name="email"
-                  value={formik.values.email}
-                  as="input"
-                  label="Email address"
-                  placeholder="company@domain.com"
-                  type="text"
-                />
-                <InputError
-                  error={formik.errors.email}
-                  touched={formik.touched.email}
-                />
-              </div>
-              <div className="mb-5">
-                <Input
-                  onChange={formik.handleChange}
-                  name="telephone"
-                  values={formik.values.telephone}
-                  as="input"
-                  label="Telephone"
-                  placeholder="Telephone"
-                  type="text"
-                />
-                <InputError
-                  error={formik.errors.telephone}
-                  touched={formik.touched.telephone}
-                />
-              </div>
+      {/* <form onSubmit={formik.handleSubmit}> */}
+      <div className="mt-14">
+        <FormLayout
+          formTitle={"Your Personal Details"}
+          onSubmit={formik.handleSubmit}
+        >
+          <div className="grid grid-cols-1 gap-x-10 sm:grid-cols-[35%,35%]">
+            <div className="mb-5">
+              <Input
+                onChange={formik.handleChange}
+                name="firstName"
+                value={formik.values.firstName}
+                as="input"
+                label="First Name"
+                placeholder="First Name"
+                type="text"
+              />
+              <InputError
+                error={formik.errors.firstName}
+                touched={formik.touched.firstName}
+              />
             </div>
-          </FormLayout>
-        </div>
-        <Devider />
-        <div className="mt-14">
-          <FormLayout formTitle={"New Customer"}>
-            <div className="grid grid-cols-1 gap-x-10 sm:grid-cols-[35%,35%]">
-              <div className="mb-5">
-                <Input
-                  onChange={formik.handleChange}
-                  name="addressOne"
-                  values={formik.values.addressOne}
-                  as="input"
-                  label="Address 1"
-                  placeholder="4279 Zboncak Port Suite 6212"
-                  type="text"
-                />
-                <InputError
-                  error={formik.errors.addressOne}
-                  touched={formik.touched.addressOne}
-                />
-              </div>
-              <div className="mb-5">
-                <Input
-                  onChange={formik.handleChange}
-                  name="addressTwo"
-                  values={formik.values.addressTwo}
-                  as="input"
-                  label="Address 2"
-                  placeholder="_"
-                  type="text"
-                />{" "}
-                <InputError
-                  error={formik.errors.addressTwo}
-                  touched={formik.touched.addressTwo}
-                />
-              </div>
-              <div className="mb-5">
-                <Input
-                  onChange={formik.handleChange}
-                  name="city"
-                  values={formik.values.city}
-                  as="input"
-                  label="City"
-                  placeholder="Your city"
-                  type="text"
-                />{" "}
-                <InputError
-                  error={formik.errors.city}
-                  touched={formik.touched.city}
-                />
-              </div>
-              <div className="mb-5">
-                <Input
-                  onChange={formik.handleChange}
-                  name="postCode"
-                  values={formik.values.postCode}
-                  as="input"
-                  label="Post Code"
-                  placeholder="05228"
-                  type="text"
-                />{" "}
-                <InputError
-                  error={formik.errors.postCode}
-                  touched={formik.touched.postCode}
-                />
-              </div>
-              <div className="mb-5">
-                <FormSelectBox
-                  Box
-                  onChange={formik.handleChange}
-                  name="division"
-                  values={formik.values.division}
-                  label="Division"
-                  id="division"
-                  options={divisions}
-                />
-                <InputError
-                  error={formik.errors.division}
-                  touched={formik.touched.division}
-                />
-              </div>
-              <div className="mb-5">
-                <FormSelectBox
-                  onChange={formik.handleChange}
-                  name="district"
-                  values={formik.values.district}
-                  label="District"
-                  id="district"
-                  options={districts}
-                />
-                <InputError
-                  error={formik.errors.district}
-                  touched={formik.touched.district}
-                />
-              </div>
+            <div className="mb-5">
+              <Input
+                onChange={formik.handleChange}
+                name="lastName"
+                value={formik.values.lastName}
+                as="input"
+                label="Last Name"
+                placeholder="Last Name"
+                type="text"
+              />
+              <InputError
+                error={formik.errors.lastName}
+                touched={formik.touched.lastName}
+              />
             </div>
-          </FormLayout>
-        </div>
-        <Devider />
-        <div className="mt-14">
-          <FormLayout formTitle={"Your Password"}>
-            <div className="grid grid-cols-1 gap-x-10 sm:grid-cols-[35%,35%]">
-              <div className="mb-5">
-                <Input
-                  onChange={formik.handleChange}
-                  name="password"
-                  values={formik.values.password}
-                  as="input"
-                  label="Password"
-                  placeholder="Password"
-                  type="text"
-                />
-                <InputError
-                  error={formik.errors.password}
-                  touched={formik.touched.password}
-                />
-              </div>
-              <div className="mb-5">
-                <Input
-                  onChange={formik.handleChange}
-                  name="repeatPassword"
-                  values={formik.values.repeatPassword}
-                  as="input"
-                  label="Repeat Password"
-                  placeholder="Repeat Password"
-                  type="text"
-                />
-                <InputError
-                  error={formik.errors.repeatPassword}
-                  touched={formik.touched.repeatPassword}
-                />
-              </div>
+            <div className="mb-5">
+              <Input
+                onChange={formik.handleChange}
+                name="email"
+                value={formik.values.email}
+                as="input"
+                label="Email address"
+                placeholder="company@domain.com"
+                type="text"
+              />
+              <InputError
+                error={formik.errors.email}
+                touched={formik.touched.email}
+              />
             </div>
-          </FormLayout>
-        </div>
-        <Devider />
+            <div className="mb-5">
+              <Input
+                onChange={formik.handleChange}
+                name="telephone"
+                values={formik.values.telephone}
+                as="input"
+                label="Telephone"
+                placeholder="Telephone"
+                type="text"
+              />
+              <InputError
+                error={formik.errors.telephone}
+                touched={formik.touched.telephone}
+              />
+            </div>
+          </div>
+        </FormLayout>
+      </div>
+      <Devider />
+      <div className="mt-14">
+        <FormLayout formTitle={"New Customer"} onSubmit={formik.handleSubmit}>
+          <div className="grid grid-cols-1 gap-x-10 sm:grid-cols-[35%,35%]">
+            <div className="mb-5">
+              <Input
+                onChange={formik.handleChange}
+                name="addressOne"
+                values={formik.values.addressOne}
+                as="input"
+                label="Address 1"
+                placeholder="4279 Zboncak Port Suite 6212"
+                type="text"
+              />
+              <InputError
+                error={formik.errors.addressOne}
+                touched={formik.touched.addressOne}
+              />
+            </div>
+            <div className="mb-5">
+              <Input
+                onChange={formik.handleChange}
+                name="addressTwo"
+                values={formik.values.addressTwo}
+                as="input"
+                label="Address 2"
+                placeholder="_"
+                type="text"
+              />{" "}
+              <InputError
+                error={formik.errors.addressTwo}
+                touched={formik.touched.addressTwo}
+              />
+            </div>
+            <div className="mb-5">
+              <Input
+                onChange={formik.handleChange}
+                name="city"
+                values={formik.values.city}
+                as="input"
+                label="City"
+                placeholder="Your city"
+                type="text"
+              />{" "}
+              <InputError
+                error={formik.errors.city}
+                touched={formik.touched.city}
+              />
+            </div>
+            <div className="mb-5">
+              <Input
+                onChange={formik.handleChange}
+                name="postCode"
+                values={formik.values.postCode}
+                as="input"
+                label="Post Code"
+                placeholder="05228"
+                type="text"
+              />{" "}
+              <InputError
+                error={formik.errors.postCode}
+                touched={formik.touched.postCode}
+              />
+            </div>
+            <div className="mb-5">
+              <FormSelectBox
+                Box
+                onChange={formik.handleChange}
+                name="division"
+                values={formik.values.division}
+                label="Division"
+                id="division"
+                options={divisions}
+              />
+              <InputError
+                error={formik.errors.division}
+                touched={formik.touched.division}
+              />
+            </div>
+            <div className="mb-5">
+              <FormSelectBox
+                onChange={formik.handleChange}
+                name="district"
+                values={formik.values.district}
+                label="District"
+                id="district"
+                options={districts}
+              />
+              <InputError
+                error={formik.errors.district}
+                touched={formik.touched.district}
+              />
+            </div>
+          </div>
+        </FormLayout>
+      </div>
+      <Devider />
+      <div className="mt-14">
+        <FormLayout formTitle={"Your Password"} onSubmit={formik.handleSubmit}>
+          <div className="grid grid-cols-1 gap-x-10 sm:grid-cols-[35%,35%]">
+            <div className="mb-5">
+              <Input
+                onChange={formik.handleChange}
+                name="password"
+                values={formik.values.password}
+                as="input"
+                label="Password"
+                placeholder="Password"
+                type="text"
+              />
+              <InputError
+                error={formik.errors.password}
+                touched={formik.touched.password}
+              />
+            </div>
+            <div className="mb-5">
+              <Input
+                onChange={formik.handleChange}
+                name="repeatPassword"
+                values={formik.values.repeatPassword}
+                as="input"
+                label="Repeat Password"
+                placeholder="Repeat Password"
+                type="text"
+              />
+              <InputError
+                error={formik.errors.repeatPassword}
+                touched={formik.touched.repeatPassword}
+              />
+            </div>
+          </div>
+        </FormLayout>
+      </div>
+      <Devider />
+      <FormLayout onSubmit={formik.handleSubmit}>
         <div className="my-16">
           <div className="mb-5 flex items-center gap-3">
             <input type="checkbox" />
@@ -310,7 +314,8 @@ const SignUp = () => {
         <Button type="submit" className={"cursor-pointer"}>
           Login
         </Button>
-      </form>
+      </FormLayout>
+      {/* </form> */}
     </Container>
   );
 };
